b6616dff20f47037a5f699057d280a4f1978e480
Remograph <Remograph@users.noreply.github.com>
2024-12-22 20:57:06 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def fake_divide(first, second):
+    if second == 0:
+        rezult = "ОШИБКА"
+    else:
+        rezult = first / second
+    return rezult
---
+++ module_4_1.py
@@ -0,0 +1,6 @@
+from fake_math import fake_divide as fk_div
+from true_math import true_divide as tr_div
+print('result1 = ', fk_div(25, 5))
+print('result2 = ', fk_div(22, 0))
+print('result3 = ', tr_div(15, 5))
+print('result4 = ', tr_div(18, 0))
